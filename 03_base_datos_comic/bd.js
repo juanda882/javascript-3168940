b8f6cbc28a1cfcd463b605e8fb5d146a70794ac7
juanda882 <juandjtabares@gmail.com>
2025-11-05 17:24:02 -0500

    base de datos creada

---
+++ 03_base_datos_comic/bd.js
@@ -0,0 +1,39 @@
+let comic = {
+  "nombreComic": "vovi a nacer",
+  "numeroEpisodios": "1",
+  "genero": "introspectivo",
+  "sinopsis": "carlos se encuentra en la playa reflexionando y se encuenytra con una vercion suya de cuando era niño",
+  "members":"carlos vives y carlos niño",
+  "autores":"juan david y maria fernanda",
+  "capitulos": [,
+  
+    {
+      "id": 1,
+      "name": "el llamdo del mar",
+      "personajes": "carlos vives y carlos niño" ,
+      "portada": ["img/..."],
+      "year": "2025",
+      "capitulos": "1",
+      "descripcion": "carlos reflexiona en la playa y se encuentra a un niño frustrado"
+    },
+    {
+      "id": 2,
+      "name": "ecos del camino",
+      "personajes": "carlos vives y carlos niño" ,
+      "portada": ["img/..."],
+      "year": "2025",
+      "capitulos": "3",
+      "descripcion": "carlos y el niño se adentran a un aventura de recuerdos"
+      
+    },
+    {
+      "id": 3,
+      "name": "el despeertar",
+      "personajes": "carlos vives y carlos niño" ,
+      "portada": ["img/..."],
+      "year": "2025",
+      "capitulos": "3",
+      "descripcion": "carlos despierta en la playa agradecido por haber vuelto a ver su proceso"
+    }
+  ]
+}
